cb745be3f53f2388ebfd08e10cca683e7b7e0868
Jesse Donat <donatj@gmail.com>
2014-08-14 17:32:52 -0500

    Header formatting updates

--- src/Header.php
+++ src/Header.php
@@ -5,8 +5,13 @@ namespace donatj\MDDom;
 class Header extends AbstractNestingElement {
 
 	public function exportMarkdown( $fragmentLevel = 0 ) {
-		return "\n\n" . str_repeat("#", $fragmentLevel + 1) . " " . parent::exportMarkdown($fragmentLevel) . "\n\n";
+		$data = str_repeat("#", $fragmentLevel + 1) . " " . parent::exportMarkdown($fragmentLevel) . "\n\n";
+
+		if($this->getPreviousSibling()) {
+			$data = "\n\n" . $data;
+		}
+
+		return $data;
 	}
 
 }
- 
