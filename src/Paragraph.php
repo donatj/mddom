84b0753dca6cc8744368f42864ecd5178ca10340
Jesse Donat <donatj@gmail.com>
2014-08-15 16:24:12 -0500

    Paragraph added

---
+++ src/Paragraph.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace donatj\MDDom;
+
+use donatj\MDDom\Interfaces\BlockElementInterface;
+
+class Paragraph extends AbstractNestingElement implements BlockElementInterface {
+	/// Hmm, this might need *something* eventually
+}
