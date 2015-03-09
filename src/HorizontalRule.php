5ce5f4bb3c74e94cef1f11eb8785e4c7267c6864
Jesse Donat <donatj@gmail.com>
2015-03-09 16:34:49 -0500

    Cleaner HR code

--- src/HorizontalRule.php
+++ src/HorizontalRule.php
@@ -7,7 +7,7 @@ use donatj\MDDom\Interfaces\BlockElementInterface;
 class HorizontalRule extends AbstractElement implements BlockElementInterface {
 
 	protected function generateMarkdown( $fragmentLevel = 0 ) {
-		return '---------------------------------------';
+		return '---';
 	}
 
 }
